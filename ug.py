5d4e45b7a539096491fe34b8b03afec3afcc9e3b
unknown <svvsvlv@gmail.com>
2021-12-15 12:57:40 +1000

    pdate

--- ug.py
+++ ug.py
@@ -3,13 +3,25 @@ from typing import Optional
 from fastapi import FastAPI
 
 app = FastAPI()
-
+tem=[]
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return tem
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/webhook")
+def webhook (item):
+    print(item)
+    return {"success": True}
+
+
+@app.post("/webhook/uplink")
+def uplink (item: dict):
+    print(item)
+    temp.append(item["uplink_message"]["decoded_payload"])
+    return {"success": True}
